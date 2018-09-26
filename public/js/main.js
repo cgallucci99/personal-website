0558dee933d8ca4c44b5f5bd665864ca588fb03f
Cam Gallucci <cgallucci99@gmail.com>
2018-09-26 18:58:31 -0400

    Added sticky headings to index, added chevron animation to heading

--- public/js/main.js
+++ public/js/main.js
@@ -1,14 +1,13 @@
 $('body').scrollspy({ target: '#scroll-spy-target' });
 $('a.page-scroll').click(function() {
-  		console.log("scroll");
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
-              scrollTop: target.offset().top
-            }, 900);
-            return false;
-          }
-        }
-      });
+	if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	  var target = $(this.hash);
+	  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	  if (target.length) {
+	    $('html,body').animate({
+	      scrollTop: target.offset().top
+	    }, 900);
+	    return false;
+	  }
+	}
+});
